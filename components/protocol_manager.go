a9a8f99236c317cfddd0647bc432690d5e5fa63d
Ramboll.Wong <ramboll.wong@hotmail.com>
2024-01-18 20:00:11 +0800

    add the implementation of the protocol manager

---
+++ components/protocol_manager.go
@@ -0,0 +1,156 @@
+package components
+
+import (
+	"errors"
+	"sync"
+
+	"github.com/rambollwong/rainbowbee/core/handler"
+	"github.com/rambollwong/rainbowbee/core/manager"
+	"github.com/rambollwong/rainbowbee/core/peer"
+	"github.com/rambollwong/rainbowbee/core/protocol"
+	"github.com/rambollwong/rainbowbee/core/store"
+	"github.com/rambollwong/rainbowcat/util"
+)
+
+var (
+	ErrProtocolIDRegistered    = errors.New("protocol id has registered")
+	ErrProtocolIDNotRegistered = errors.New("protocol id is not registered")
+
+	_ manager.ProtocolManager = (*ProtocolManager)(nil)
+)
+
+// ProtocolManager manages protocols and protocol message handlers for peers.
+type ProtocolManager struct {
+	mu                  sync.RWMutex
+	localPID            peer.ID
+	handlers            map[protocol.ID]handler.MsgPayloadHandler
+	protocolBook        store.ProtocolBook
+	supportedCallback   manager.ProtocolSupportNotifyFunc
+	unsupportedCallback manager.ProtocolSupportNotifyFunc
+}
+
+// RegisterMsgPayloadHandler registers a protocol and associates a handler.MsgPayloadHandler with it.
+// It returns an error if the registration fails.
+func (p *ProtocolManager) RegisterMsgPayloadHandler(protocolID protocol.ID, handler handler.MsgPayloadHandler) error {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+	_, ok := p.handlers[protocolID]
+	if ok {
+		return ErrProtocolIDRegistered
+	}
+	p.handlers[protocolID] = handler
+	return nil
+}
+
+// UnregisterMsgPayloadHandler unregisters a previously registered protocol.
+// It returns an error if the un-registration fails.
+func (p *ProtocolManager) UnregisterMsgPayloadHandler(protocolID protocol.ID) error {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+	_, ok := p.handlers[protocolID]
+	if !ok {
+		return ErrProtocolIDNotRegistered
+	}
+	delete(p.handlers, protocolID)
+	return nil
+}
+
+// Registered checks if a protocol is registered and supported.
+// It returns true if the protocol is supported, and false otherwise.
+func (p *ProtocolManager) Registered(protocolID protocol.ID) bool {
+	p.mu.RLock()
+	defer p.mu.RUnlock()
+	_, ok := p.handlers[protocolID]
+	return ok
+}
+
+// Handler returns the message payload handler associated with the registered protocol.
+// If the protocol is not registered or supported, it returns nil.
+func (p *ProtocolManager) Handler(protocolID protocol.ID) handler.MsgPayloadHandler {
+	p.mu.RLock()
+	defer p.mu.RUnlock()
+	h, _ := p.handlers[protocolID]
+	return h
+}
+
+// SupportedByPeer checks if a peer with the given peer.ID supports a specific protocol.
+// It returns true if the protocol is supported by the peer, and false otherwise.
+// If the peer is not connected, it returns false.
+func (p *ProtocolManager) SupportedByPeer(pid peer.ID, protocolID protocol.ID) bool {
+	p.mu.RLock()
+	defer p.mu.RUnlock()
+	return p.protocolBook.ContainsProtocol(pid, protocolID)
+}
+
+// SupportedProtocolsOfPeer returns a list of protocol.IDs that are supported by the peer with the given peer.ID.
+// If the peer is not connected or doesn't support any protocols, it returns an empty list.
+func (p *ProtocolManager) SupportedProtocolsOfPeer(pid peer.ID) []protocol.ID {
+	p.mu.RLock()
+	defer p.mu.RUnlock()
+	return p.protocolBook.GetProtocols(pid)
+}
+
+// notifyIfChanged notifies the registered callback functions if there are changes in the supported protocols of a peer.
+// It is called internally by SetSupportedProtocolsOfPeer method.
+func (p *ProtocolManager) notifyIfChanged(pid peer.ID, protocols []protocol.ID) {
+	if len(protocols) == 0 {
+		return
+	}
+
+	if p.supportedCallback != nil {
+		for _, ID := range protocols {
+			if !p.protocolBook.ContainsProtocol(pid, ID) {
+				p.supportedCallback(ID, pid)
+			}
+		}
+	}
+
+	if p.unsupportedCallback != nil {
+		supported := p.protocolBook.GetProtocols(pid)
+		unsupported := util.SliceExcludeAll(supported, protocols...)
+		for _, ID := range unsupported {
+			p.unsupportedCallback(ID, pid)
+		}
+	}
+}
+
+// SetSupportedProtocolsOfPeer stores the protocols supported by the peer with the given peer.ID.
+// It notifies the registered callback functions if there are changes in the supported protocols of the peer.
+// It returns an error if the protocol storage fails.
+func (p *ProtocolManager) SetSupportedProtocolsOfPeer(pid peer.ID, protocolIDs []protocol.ID) error {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+	p.notifyIfChanged(pid, protocolIDs)
+	return p.protocolBook.SetProtocols(pid, protocolIDs)
+}
+
+// CleanPeer removes all records of protocols supported by the peer with the given peer.ID.
+// It notifies the registered callback functions for any unsupported protocols.
+// It returns an error if the cleanup fails.
+func (p *ProtocolManager) CleanPeer(pid peer.ID) error {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+	supported := p.protocolBook.GetProtocols(pid)
+	for _, ID := range supported {
+		p.unsupportedCallback(ID, pid)
+	}
+	return p.protocolBook.ClearProtocol(pid)
+}
+
+// SetProtocolSupportedNotifyFunc sets the callback function to be invoked when a protocol is supported by a peer.
+// It returns an error if the setting fails.
+func (p *ProtocolManager) SetProtocolSupportedNotifyFunc(notifyFunc manager.ProtocolSupportNotifyFunc) error {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+	p.supportedCallback = notifyFunc
+	return nil
+}
+
+// SetProtocolUnsupportedNotifyFunc sets the callback function to be invoked when a protocol is unsupported by a peer.
+// It returns an error if the setting fails.
+func (p *ProtocolManager) SetProtocolUnsupportedNotifyFunc(notifyFunc manager.ProtocolSupportNotifyFunc) error {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+	p.unsupportedCallback = notifyFunc
+	return nil
+}
--- core/manager/protocol_manager.go
+++ core/manager/protocol_manager.go
@@ -10,60 +10,67 @@ import (
 // ProtocolSupportNotifyFunc is a function type used to notify whether a peer supports a protocol.
 type ProtocolSupportNotifyFunc func(protocolID protocol.ID, pid peer.ID)
 
-// ProtocolManager manages all protocols and protocol message handlers for all peers.
+// ProtocolManager manages protocols and protocol message handlers for peers.
 type ProtocolManager interface {
-	// RegisterMsgPayloadHandler registers a protocol supported by us
-	// and maps a handler.MsgPayloadHandler to this protocol.
+	// RegisterMsgPayloadHandler registers a protocol and associates a handler.MsgPayloadHandler with it.
+	// It returns an error if the registration fails.
 	RegisterMsgPayloadHandler(protocolID protocol.ID, handler handler.MsgPayloadHandler) error
 
-	// UnregisterMsgPayloadHandler unregisters a protocol supported by us.
+	// UnregisterMsgPayloadHandler unregisters a previously registered protocol.
+	// It returns an error if the un-registration fails.
 	UnregisterMsgPayloadHandler(protocolID protocol.ID) error
 
-	// IsRegistered returns whether a protocol given is supported by us.
-	IsRegistered(protocolID protocol.ID) bool
+	// Registered checks if a protocol is registered and supported.
+	// It returns true if the protocol is supported, and false otherwise.
+	Registered(protocolID protocol.ID) bool
 
-	// GetHandler returns the handler.MsgPayloadHandler mapped to the protocol supported by us and the ID is the given protocolID.
-	// If the protocol is not supported by us, it returns nil.
-	GetHandler(protocolID protocol.ID) handler.MsgPayloadHandler
+	// Handler returns the message payload handler associated with the registered protocol.
+	// If the protocol is not registered or supported, it returns nil.
+	Handler(protocolID protocol.ID) handler.MsgPayloadHandler
 
-	// GetSelfSupportedProtocols returns a list of protocol.IDs that are supported by ourselves.
-	GetSelfSupportedProtocols() []protocol.ID
+	// SupportedByPeer checks if a peer with the given peer.ID supports a specific protocol.
+	// It returns true if the protocol is supported by the peer, and false otherwise.
+	// If the peer is not connected, it returns false.
+	SupportedByPeer(pid peer.ID, protocolID protocol.ID) bool
 
-	// IsPeerSupported returns whether the protocol is supported by the peer with the given peer.ID.
-	// If the peer is not connected to us, it returns false.
-	IsPeerSupported(pid peer.ID, protocolID protocol.ID) bool
+	// SupportedProtocolsOfPeer returns a list of protocol.IDs that are supported by the peer with the given peer.ID.
+	// If the peer is not connected or doesn't support any protocols, it returns an empty list.
+	SupportedProtocolsOfPeer(pid peer.ID) []protocol.ID
 
-	// GetPeerSupportedProtocols returns a list of protocol.IDs that are supported by the peer with the given peer.ID.
-	GetPeerSupportedProtocols(pid peer.ID) []protocol.ID
+	// SetSupportedProtocolsOfPeer stores the protocols supported by the peer with the given peer.ID.
+	SetSupportedProtocolsOfPeer(pid peer.ID, protocolIDs []protocol.ID) error
 
-	// SetPeerSupportedProtocols stores the protocols supported by the peer with the given peer.ID.
-	SetPeerSupportedProtocols(pid peer.ID, protocolIDs []protocol.ID)
+	// CleanPeer removes all records of protocols supported by the peer with the given peer.ID.
+	CleanPeer(pid peer.ID) error
 
-	// CleanPeerSupportedProtocols removes all records of protocols supported by the peer with the given peer.ID.
-	CleanPeerSupportedProtocols(pid peer.ID)
+	// SetProtocolSupportedNotifyFunc sets a function to be called when a peer supports a protocol.
+	// The function is called with the peer.ID of the supporting peer and the supported protocol ID.
+	SetProtocolSupportedNotifyFunc(notifyFunc ProtocolSupportNotifyFunc) error
 
-	// SetProtocolSupportedNotifyFunc sets a function for notifying when a peer supports a protocol.
-	SetProtocolSupportedNotifyFunc(notifyFunc ProtocolSupportNotifyFunc)
-
-	// SetProtocolUnsupportedNotifyFunc sets a function for notifying when a peer no longer supports a protocol.
-	SetProtocolUnsupportedNotifyFunc(notifyFunc ProtocolSupportNotifyFunc)
+	// SetProtocolUnsupportedNotifyFunc sets a function to be called when a peer no longer supports a protocol.
+	// The function is called with the peer.ID of the peer and the unsupported protocol ID.
+	SetProtocolUnsupportedNotifyFunc(notifyFunc ProtocolSupportNotifyFunc) error
 }
 
-// ProtocolExchanger is usually used to exchange protocols supported by both peers.
+// ProtocolExchanger is responsible for exchanging protocols supported by both peers.
 type ProtocolExchanger interface {
 	// ProtocolID returns the protocol.ID of the exchanger service.
-	// The protocol ID will be registered in the host.RegisterMsgPayloadHandler method.
+	// The protocol ID will be registered using the host.RegisterMsgPayloadHandler method.
 	ProtocolID() protocol.ID
 
 	// Handle returns the message payload handler of the exchanger service.
-	// It will be registered in the host.Host.RegisterMsgPayloadHandler method.
+	// It will be registered using the host.Host.RegisterMsgPayloadHandler method.
 	Handle() handler.MsgPayloadHandler
 
-	// ExchangeProtocol sends the protocols supported by us to the other peer and receives the protocols supported by the other peer.
+	// ExchangeProtocol sends the protocols supported by the local peer to the remote peer and receives the protocols supported by the remote peer.
 	// This method is invoked during connection establishment.
+	// It takes a network.Connection parameter representing the connection between the local and remote peer.
+	// It returns a slice of protocol.IDs supported by the remote peer, or an error if the protocol exchange fails.
 	ExchangeProtocol(conn network.Connection) ([]protocol.ID, error)
 
-	// PushProtocols sends the protocols supported by us to the other peer.
+	// PushProtocols sends the protocols supported by the local peer to the remote peer.
 	// This method is invoked when a new protocol is registered.
+	// It takes a peer.ID parameter representing the remote peer ID to which the protocols will be pushed.
+	// It returns an error if the protocol push fails.
 	PushProtocols(pid peer.ID) error
 }
